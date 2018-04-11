19c3249ff6f9a7a67ee50926c375f25289f22a0d
Felipe Mendes <mendesfreela@gmail.com>
2018-04-11 10:55:48 -0300

    'Commitsystemamultiplefile'

---
+++ UploadFile/UploadFile/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UploadFile.Models;
+
+namespace UploadFile.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Remessa arq)
+        {
+            try
+            {
+                string nomeArquivo = "";
+                string arquivoEnviados = "";
+
+                foreach (var item in arq.Arquivos)
+                {
+                    if(item.ContentLength > 0)
+                    {
+                        nomeArquivo = Path.GetFileName(item.FileName);
+                        var caminho = Path.Combine(Server.MapPath("~/Imagens"), nomeArquivo);
+                        item.SaveAs(caminho);
+                    }
+                    arquivoEnviados = arquivoEnviados + "," + nomeArquivo;
+                }
+                ViewBag.Mensagem = "Arquivos enviados: " + arquivoEnviados + "com sucesso.";
+            }
+            catch (Exception ex)
+            {
+
+
+                ViewBag.Mensagem = "Erro:"+ex.Message;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ UploadFile/UploadFile/Models/Remessa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UploadFile.Models
+{
+    public class Remessa
+    {
+        public IEnumerable<HttpPostedFileBase> Arquivos { get; set; }
+    }
+}
